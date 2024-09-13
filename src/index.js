d2260e5705458e8e055bc2b22671ba850b3631e8
TLucjd <tanluc.jdtech04@gmail.com>
2024-09-13 13:56:24 +0700

    demo

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux"
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
+import store from "./redux/reducer/store";
 
 
 ReactDOM.render(
+  // <Provider store={store}>
   <BrowserRouter>
     <Layout />
   </BrowserRouter>,
   document.getElementById("root")
+
 );
 
 // If you want to start measuring performance in your app, pass a function
